6d5445027c302457413006e4b301db28e8329f3a
sbkimxtheia <murumuru4633@gmail.com>
2022-12-28 22:47:48 +0900

    Update Ink.java

--- src/main/java/kr/sbxt/xtheia/theia/ink/Ink.java
+++ src/main/java/kr/sbxt/xtheia/theia/ink/Ink.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.Server;
 import org.bukkit.inventory.ItemFactory;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ink extends JavaPlugin
@@ -24,6 +25,12 @@ public final class Ink extends JavaPlugin
 		
 		log(Comp.tc("Ink Enabled Successfully!", Colors.LEGACY_AQUA));
 		log(Comp.a(Comp.t("Powered by "), Comp.tc("THEIA Core", Colors.LEGACY_YELLOW), Comp.t(" from "), Comp.tc("XTHEIA", Colors.LEGACY_AQUA)));
+	}
+	
+	public static void logPluginLoaded(Plugin plugin)
+	{
+		log(Comp.tc(plugin.getName() + " Enabled Successfully!", Colors.LEGACY_AQUA));
+		log(Comp.a(Comp.t("Powered by "), Comp.tc("Ink", Colors.LEGACY_YELLOW), Comp.t(" from "), Comp.tc("XTHEIA", Colors.LEGACY_AQUA)));
 	}
 	
 	public static Server getCurrentServer()
